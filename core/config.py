87d1823d5393a7a17c9c8fe28b80eba201fc5d1c
YUHANG ZHAO <zhaoy36@mcmaster.ca>
2020-12-20 22:11:09 -0500

    Upload essential files

---
+++ core/config.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+# coding=utf-8
+from easydict import EasyDict as edict
+
+
+__C                           = edict()
+# Consumers can get config by: from config import cfg
+
+cfg                           = __C
+
+# iou threshold
+__C.iou                       = 0.45
+# score threshold
+__C.score                     = 0.25
+# resize images to
+__C.size                      = 416
+# codec used in VideoWriter when saving video to file
+__C.output_format             = 'XVID'
+# path to weights file
+__C.weights                   = './yolov4-416'
+# path to input video
+__C.video                     = './data/road.mp4'
+# path to output video
+__C.output                    = './results.avi'
+
+#==========================================================
+# YOLO options
+__C.YOLO                      = edict()
+
+__C.YOLO.CLASSES              = "./data/classes/coco.names"
+__C.YOLO.ANCHORS              = [12,16, 19,36, 40,28, 36,75, 76,55, 72,146, 142,110, 192,243, 459,401]
+__C.YOLO.STRIDES              = [8, 16, 32]
+__C.YOLO.XYSCALE              = [1.2, 1.1, 1.05]
---
+++ detectvideo.py
@@ -0,0 +1,91 @@
+import time
+import tensorflow as tf
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+from absl import app
+import core.utils as utils
+from tensorflow.python.saved_model import tag_constants
+from PIL import Image
+import cv2
+import numpy as np
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+from core.config import cfg
+
+def main(_argv):
+
+    config = ConfigProto()
+    config.gpu_options.allow_growth = True
+    session = InteractiveSession(config=config)
+    STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config()
+    input_size = cfg.size
+    video_path = cfg.video
+
+    print("Video from: ", video_path )
+    vid = cv2.VideoCapture(video_path)
+
+    saved_model_loaded = tf.saved_model.load(cfg.weights, tags=[tag_constants.SERVING])
+    infer = saved_model_loaded.signatures['serving_default']    
+    
+    # by default VideoCapture returns float instead of int
+    width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = int(vid.get(cv2.CAP_PROP_FPS))
+    codec = cv2.VideoWriter_fourcc(*cfg.output_format)
+    out = cv2.VideoWriter(cfg.output, codec, fps, (width, height))
+
+    frame_id = 0
+    while True:
+        return_value, frame = vid.read()
+        if return_value:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image = Image.fromarray(frame)
+        else:
+            if frame_id == vid.get(cv2.CAP_PROP_FRAME_COUNT):
+                print("Video processing complete")
+                break
+            raise ValueError("No image! Try with another video format")
+        
+        frame_size = frame.shape[:2]
+        image_data = cv2.resize(frame, (input_size, input_size))
+        image_data = image_data / 255.
+        image_data = image_data[np.newaxis, ...].astype(np.float32)
+        prev_time = time.time()
+        
+        batch_data = tf.constant(image_data)
+        pred_bbox = infer(batch_data)
+
+        for key, value in pred_bbox.items():
+          boxes = value[:, :, 0:4]
+          pred_conf = value[:, :, 4:]
+
+        boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
+            boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
+            scores=tf.reshape(
+                pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
+            max_output_size_per_class=50,
+            max_total_size=50,
+            iou_threshold = cfg.iou,
+            score_threshold = cfg.score
+        )
+
+        pred_bbox = [boxes.numpy(), scores.numpy(), classes.numpy(), valid_detections.numpy()]
+        image = utils.draw_bbox(frame, pred_bbox)
+        curr_time = time.time()
+        exec_time = curr_time - prev_time
+        result = np.asarray(image)
+        info = "time: %.2f ms" %(1000*exec_time)
+        print(info)
+
+        result = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
+        out.write(result)
+
+        frame_id += 1
+
+if __name__ == '__main__':
+    try:
+        app.run(main)
+    except SystemExit:
+        pass
