52962333d602273f01effeb993c839f9275f5045
YUHANG ZHAO <52333379+yuhang2685@users.noreply.github.com>
2020-12-22 11:29:20 -0500

    Change input output config to command line args

--- detectimage.py
+++ detectimage.py
@@ -5,28 +5,20 @@ if len(physical_devices) > 0:
 from absl import app, flags, logging
 from absl.flags import FLAGS
 import core.utils as utils
-#from core.yolov4 import filter_boxes
 from tensorflow.python.saved_model import tag_constants
 from PIL import Image
 import cv2
 import numpy as np
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
-
-#flags.DEFINE_string('framework', 'tf', '(tf, tflite, trt')
-flags.DEFINE_string('weights', './checkpoints/yolov4-416',
-                    'path to weights file')
-#flags.DEFINE_integer('size', 416, 'resize images to')
-#flags.DEFINE_boolean('tiny', False, 'yolo or yolo-tiny')
-#flags.DEFINE_string('model', 'yolov4', 'yolov3 or yolov4')
-flags.DEFINE_string('image', './data/kite.jpg', 'path to input image')
-flags.DEFINE_string('output', 'result.png', 'path to output image')
-#flags.DEFINE_float('iou', 0.45, 'iou threshold')
-#flags.DEFINE_float('score', 0.25, 'score threshold')
-
 from core.config import cfg
 
+flags.DEFINE_string('image', './data/kite.jpg', 'path to input image')
+flags.DEFINE_string('output', 'result.png', 'path to output image')
+
 def main(_argv):
+
+    # Setup configs
     config = ConfigProto()
     config.gpu_options.allow_growth = True
     session = InteractiveSession(config=config)
@@ -34,6 +26,7 @@ def main(_argv):
     input_size = cfg.size
     image_path = FLAGS.image
 
+    # Load image and preprocess
     original_image = cv2.imread(image_path)
     original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
 
@@ -47,7 +40,8 @@ def main(_argv):
         images_data.append(image_data)
     images_data = np.asarray(images_data).astype(np.float32)
 
-    saved_model_loaded = tf.saved_model.load(FLAGS.weights, tags=[tag_constants.SERVING])
+    # Load pre-trained weights and apply object detection
+    saved_model_loaded = tf.saved_model.load(cfg.weights, tags=[tag_constants.SERVING])
     infer = saved_model_loaded.signatures['serving_default']
     batch_data = tf.constant(images_data)
     pred_bbox = infer(batch_data)
@@ -55,6 +49,7 @@ def main(_argv):
         boxes = value[:, :, 0:4]
         pred_conf = value[:, :, 4:]
 
+    # Perform NMS after YOLOv4 inference
     boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
         boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
         scores=tf.reshape(
@@ -68,6 +63,8 @@ def main(_argv):
     image = utils.draw_bbox(original_image, pred_bbox)
     # image = utils.draw_bbox(image_data*255, pred_bbox)
     image = Image.fromarray(image.astype(np.uint8))
+
+    # Output result
     image.show()
     image = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
     cv2.imwrite(FLAGS.output, image)
